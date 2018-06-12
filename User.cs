8f8780990b420da61eedf752272cbc20c8c9b2fa
Stephen Burns <32389562+Burnzys@users.noreply.github.com>
2018-06-12 12:18:13 +0100

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name_ID { get; set; }
+        public int Phone_ID { get; set; }
+        public string Emial_ID { get; set; }
+        public int Address_ID { get; set; }
+        public int City_ID { get; set; }
+        public int County_ID { get; set; }
+        public int SocialM_ID { get; set; }
+        public bool admin { get; set; }
+
+    }
+}
